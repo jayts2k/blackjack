cbe2682030b974627bacfd3d2b2e448ae3eb419d
jayts2k <49611494+jayts2k@users.noreply.github.com>
2019-05-09 15:29:48 +0100

    1st Update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,14 @@
-/* Code goes here */
+/* 
+    Title: Blackjack
+    Author: Johnny Smith
+    Date: 09/05/2019
+*/
+
+let card1 = 'Ace of Spades',
+    card2 = 'Ten of Hearts';
+
+console.log('Welcome to Blackjack!');
+
+console.log('You are dealt: ');
+console.log(' ' + card1);
+console.log(' ' + card2)
