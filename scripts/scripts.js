49fa694952462b787630b472ca4192822878e549
jayts2k <49611494+jayts2k@users.noreply.github.com>
2019-05-09 16:35:19 +0100

    Module Finished

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,22 +5,29 @@
 */
 
 let suits = ['Hearts','Clubs','Diamonds','Spades'],
-    values = ['Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace'],
-    deck = []
+    values = ['Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace']
 
-for (let suitIdx=0; suitIdx < suits.length; suitIdx++) {
-    for (let valueIdx=0; valueIdx < values.length; valueIdx++) {
-        deck.push(values[valueIdx] + ' of ' + suits[suitIdx])
+
+function createDeck() {
+    let deck = [];
+    for (let suitIdx=0; suitIdx < suits.length; suitIdx++) {
+        for (let valueIdx=0; valueIdx < values.length; valueIdx++) {
+            deck.push(values[valueIdx] + ' of ' + suits[suitIdx])
+        }
     }
+    return deck;
 }
 
-let playerCards = [deck[0], deck[1]]
+function getNextCard() {
+    return deck.shift();
+}
 
+let deck = createDeck();
+let playerCards = [getNextCard(), getNextCard()];
 let totalChips = 1000,
     totalBet = [20, 40, 60, 80, 100];
 
 console.log('Welcome to Blackjack!');
-
 console.log('You are dealt: ');
 console.log(' ' + playerCards[0]);
 console.log(' ' + playerCards[1]);
