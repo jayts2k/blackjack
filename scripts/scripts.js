0618bf4aae6c5e0d049852afe752504ca3fe2549
jayts2k <49611494+jayts2k@users.noreply.github.com>
2019-05-09 16:05:33 +0100

    Module Finished

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,11 +4,19 @@
     Date: 09/05/2019
 */
 
-let card1 = 'Ace of Spades',
-    card2 = 'Ten of Hearts';
+let deck = [
+    'Ace of Spades',
+    'Two of Spades',
+    'Three of Spades'
+];
+
+let playerCards = [deck[0], deck[1]]
+
+let totalChips = 1000,
+    totalBet = [20, 40, 60, 80, 100];
 
 console.log('Welcome to Blackjack!');
 
 console.log('You are dealt: ');
-console.log(' ' + card1);
-console.log(' ' + card2)
+console.log(' ' + playerCards[0]);
+console.log(' ' + playerCards[1]);
